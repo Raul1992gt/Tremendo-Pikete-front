e47d32227ff6ec94e67eb072f7dd9af51d1140c4
Raul <rgarciat94@gmai.com>
2025-02-16 12:13:28 +0100

    Dejar sólo mensaje al logear

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -16,35 +16,6 @@ const HomePage = () => {
       <Typography variant="h3" gutterBottom>
         {nombreUsuario ? `Bienvenido, ${nombreUsuario}!` : 'Bienvenidos a la aplicación de gestión de jugadores'}
       </Typography>
-      <Typography variant="h6">
-        Haz clic en un enlace para ver más opciones:
-      </Typography>
-      
-      {/* Usar Stack para separar los botones verticalmente */}
-      <Stack spacing={2} direction="column" alignItems="center">
-        <Box width="200px">
-          <Button
-            variant="contained"
-            color="primary"
-            href="/roster" // No pasa el id, solo va a Roster
-            size="small"
-            fullWidth
-          >
-            Ver Roster
-          </Button>
-        </Box>
-        {/* <Box width="200px">
-          <Button
-            variant="contained"
-            color="primary"
-            href={`/dashboard/${userId}`} // Pasa el id en la URL solo para Dashboard
-            size="small"
-            fullWidth
-          >
-            Mi Dashboard
-          </Button>
-        </Box> */}
-      </Stack>
     </Container>
   );
 };
